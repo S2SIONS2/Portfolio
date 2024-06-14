3032b2031d765d50754ae2880a1bce90e560b499
Sion <solee9802@gmail.com>
2024-06-15 00:05:59 +0900

    프로젝트 부분 완료 컨택트 퍼블리싱 완료

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,61 @@
+import './Contact.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faBlog } from '@fortawesome/free-solid-svg-icons'
+import { faPhone } from '@fortawesome/free-solid-svg-icons'
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
+const toastMsg = () => {
+    const msg = document.querySelector('.Toastify');
+
+    msg.style.display = "block"
+    setTimeout(function(){
+        msg.style.display = "none";
+    }, 2000)
+}
+
 const Contact = () => {
     return(
-        <div className="Contact">contact</div>
+        <div className="Contact">
+            <div className='title'>CONTACT</div>
+            <div className='contact_wrap'>
+                <div className="wrap">
+                    <div className="logo">
+                        <FontAwesomeIcon icon={faGithub} size='3x'/>
+                        <h1>GitHub :</h1>
+                    </div>
+                    <h2>S2SIONS2</h2>
+                </div>
+                <div className='wrap'>
+                    <div className='logo'>
+                        <FontAwesomeIcon icon={faPhone} size='3x'/>
+                        <h1>Phone :</h1>
+                    </div>
+                    <h2>010-2371-1933</h2>
+                </div>
+                <div className='wrap'>
+                    <div className='logo'>
+                        <FontAwesomeIcon icon={faEnvelope} size='3x' />
+                        <h1>E-mail :</h1>
+                    </div>
+                    <h2>chasio100@naver.com</h2>
+                    <CopyToClipboard text="chasio100@naver.com">
+                        <button onClick={toastMsg}>복사하기</button>
+                    </CopyToClipboard>
+                    <div className='Toastify'>
+                        클립보드에 복사되었습니다.
+                    </div>
+                </div>
+                <div className='wrap'>
+                    <div className='logo'>
+                        <FontAwesomeIcon icon={faBlog} size='3x'/>
+                        <h1>Blog: </h1>
+                    </div>
+                    <h2>https://deer-develope-diary.tistory.com/</h2>
+                </div>
+            </div>       
+        </div>
     )
 }
 
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,6 +1,30 @@
+import './Projects.css'
+import tagWords from  '../assets/tagWords.png';
+
 const Projects = () => {
     return(
-        <div className="Projects">projects</div>
+        <div className="Projects">
+            <div className='title'>PROJECTS</div>
+            <div className='projects_wrap'>
+                <div className='project_card'>
+                    <div className='imgArea'>
+                        <div style={{background: `url(${tagWords}) no-repeat center/cover`, width:'100%', height: '100%', display: 'block'}}></div>
+                    </div>
+                    <div className='infoArea'>
+                        <div className='project_title'>
+                            끝 말 잇기
+                        </div>
+                        <div className='project_info'>
+                            국립국어원 API를 활용한 끝말잇기 게임
+                            <div className='btn_wrap'>
+                                <button type='button'> 자세히 보기 </button>
+                                <button type='button'> GitHub 바로가기 </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
